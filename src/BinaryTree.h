9838a14665a73fd420347b5f180e5fea19855302
Kevin <kevinhmai@outlook.com>
2019-11-16 22:55:11 -0800

    didnt delete extrafiles yet commit

--- src/BinaryTree.h
+++ src/BinaryTree.h
@@ -3,6 +3,7 @@
 #include "Tree.h"
 #include "TreeNode.h"
 #include <vector>
+#include <iostream>
 #include "LinkedStack.h"
 
 template<class T>
@@ -41,13 +42,30 @@ public:
         return false;
     }
 
-    std::vector<T> traverseInOrder() override {
-
+    std::vector<T> traverseInOrder() override {//left, root, right
+    //https://github.com/pdgetrf/UWB_CSS342_2019_Fall/blob/master/class8/tree/src/BinaryTree.h
         // homework, to be done iteratively
-        left
-        root
-        right
-        
+        LinkedStack<TreeNode<T> *> stack;
+        TreeNode<T>* curr=root;
+        std::vector<T> output;
+        while(curr!=nullptr || !stack.isEmpty()) {
+
+            // travel as left as possible
+            while(curr!= nullptr){
+                stack.push(curr);
+                curr=curr->left;
+            }
+
+            // the most left node
+            // this could also be the node in the middle
+            curr=stack.peek();
+            stack.pop();
+            output.push_back(curr->val);
+
+            // move to the right side
+            curr=curr->right;
+        }
+        return output;
     }
 
     std::vector<T> traversePreOrder() override {
@@ -56,23 +74,98 @@ public:
 
     std::vector<T> traversePostOrder() override {//iteratively using a stack
         // homework, to be done iteratively
-        Stack<TreeNode<T>> stack;//og stack
-        stack.push(root);
+        //left, right, root
+        //https://www.techiedelight.com/postorder-tree-traversal-iterative-recursive/
+        LinkedStack<TreeNode<T>*> stack;//og stack
+        LinkedStack<TreeNode<T>*> temp;//temp stack to help reverse
+        TreeNode<T>* curr=root;
+        std::vector<T> output;
+
+        if (root==nullptr) {
+            return output;
+        }
+        stack.push(root);//push root since it's last, bottom of stack
 
         while(!stack.isEmpty()){
-            ListNode<T> *curr=stack.top();
+            curr=stack.peek();
             stack.pop();
+
+            temp.push(curr);
+
+            if(curr->left){
+                stack.push(curr->left);
+            }
+            if(curr->right){
+                stack.push(curr->right);
+            }
         }
+        while(!temp.isEmpty()){//reverses order
+            curr=temp.peek();
+            temp.pop();
+            output.push_back(curr->val);
+        }
+        return output;
 
     }
 
     virtual ~BinaryTree() {//using delete function to delete connections
         // homework
-        deleteTree(root);//calls to do recursively
+        //deleteTree(root);//calls to do recursively
+        //decided not to risk it and use the destructor you already made
+        LinkedStack<TreeNode<T> *> stack;
+
+        if (root == nullptr) {
+            return;
+        }
+
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            TreeNode<T> *node = stack.peek();
+            stack.pop();
+
+            delete node;//root
+
+            if (node->left != nullptr) {//left
+                stack.push(node->left);
+            }
+
+            if (node->right != nullptr) {//right
+                stack.push(node->right);
+            }
+
+        }
+    }
+
+    TreeNode<T>* _LCA(TreeNode<T>* node, T value1, T value2) {
+        if (node== nullptr) {
+            return nullptr;
+        }
+
+        // check root
+        if (node->val == value1 || node->val == value2) {
+            return node;
+        }
+
+        // check both childrens
+        TreeNode<T>* left = _LCA(node->left, value1, value2);
+        TreeNode<T>* right = _LCA(node->right, value1, value2);
+
+        if (left!=nullptr && right!=nullptr) {
+            return node;
+        }
+
+        if (left==nullptr && right==nullptr) {
+            return nullptr;
+        }
+
+        return left == nullptr ? right : left;
     }
 
     T LCA(T node1, T node2) {
         // homework
+        TreeNode<T>* node = _LCA(root, node1, node2);
+        return node == nullptr ? -1 : node->val;
     }
 
     bool add(const T &) override {
--- test/BinaryTreeTest.cpp
+++ test/BinaryTreeTest.cpp
@@ -2,6 +2,11 @@
 #include "gtest/gtest.h"
 
 TEST(tree, demo) {
+    //        3
+    //      /   \
+    //     1     5
+    //   /  \   /
+    //  2   7  5
     TreeNode<int>* root = new TreeNode<int>(3);
     root->setLeft(new TreeNode<int>(1));
     root->setRight(new TreeNode<int>(5));
@@ -16,7 +21,93 @@ TEST(tree, demo) {
     ASSERT_EQ(tree.height(),3);
 }
 
-TEST(tree, preDeconstruct){
+/*//no deconstructor test, as long as no memleak
+TEST(tree, deconstructInPre){
+    TreeNode<int>* root = new TreeNode<int>(3);
+    root->setLeft(new TreeNode<int>(1));
+    root->setRight(new TreeNode<int>(5));
 
+    root->getLeft()->setLeft(new TreeNode<int>(2));
+    root->getLeft()->setRight(new TreeNode<int>(7));
+
+    root->getRight()->setLeft(new TreeNode<int>(5));
+    BinaryTree<int> tree(root);
+    //~BinaryTree();
+}*/
+
+TEST(traverse,inOrder){
+    //        3
+    //      /   \
+    //     1     5
+    //   /  \   /
+    //  2   7  5
+    BinaryTree<int> empty(nullptr);
+    std::vector<int> emptyTraverse=empty.traverseInOrder();
+    std::vector<int> expectedEmptyResult;
+    ASSERT_TRUE(emptyTraverse==expectedEmptyResult);
+
+    TreeNode<int>* root = new TreeNode<int>(3);
+    root->setLeft(new TreeNode<int>(1));
+    root->setRight(new TreeNode<int>(5));
+
+    root->getLeft()->setLeft(new TreeNode<int>(2));
+    root->getLeft()->setRight(new TreeNode<int>(7));
+
+    root->getRight()->setLeft(new TreeNode<int>(5));
+
+    BinaryTree<int> tree(root);
+    std::vector<int> expectedResult={2,1,7,3,5,5};
+
+    std::vector<int> theResult=tree.traverseInOrder();
+
+    ASSERT_EQ(theResult,expectedResult);
+}
+
+TEST(traverse, postOrder){
+    //        3
+    //      /   \
+    //     1     5
+    //   /  \   /
+    //  2   7  5
+    TreeNode<int>* root = new TreeNode<int>(3);
+    root->setLeft(new TreeNode<int>(1));
+    root->setRight(new TreeNode<int>(5));
+
+    root->getLeft()->setLeft(new TreeNode<int>(2));
+    root->getLeft()->setRight(new TreeNode<int>(7));
+
+    root->getRight()->setLeft(new TreeNode<int>(5));
+
+    BinaryTree<int> tree(root);
+    std::vector<int> expectedResult={2,7,1,5,5,3};
+
+    std::vector<int> theResult=tree.traversePostOrder();
+
+    ASSERT_EQ(theResult,expectedResult);
+}
+
+TEST(least, leastCommon){
+    //        4
+    //      /   \
+    //     8     6
+    //   /  \   / \
+    //  7   3  2   9
+    TreeNode<int>* root = new TreeNode<int>(4);
+    root->setLeft(new TreeNode<int>(8));
+    root->setRight(new TreeNode<int>(6));
+
+    root->getLeft()->setLeft(new TreeNode<int>(7));
+    root->getLeft()->setRight(new TreeNode<int>(3));
+
+    root->getRight()->setLeft(new TreeNode<int>(2));
+    root->getRight()->setRight(new TreeNode<int>(9));
+
+    BinaryTree<int> tree(root);
+    ASSERT_TRUE(tree.LCA(4, 4) == 4);
+    ASSERT_TRUE(tree.LCA(7, 7) == 7);
+    ASSERT_TRUE(tree.LCA(7, 3) == 8);
+    ASSERT_TRUE(tree.LCA(7, 8) == 8);
+    ASSERT_TRUE(tree.LCA(8, 6) == 4);
+    ASSERT_TRUE(tree.LCA(3, 2) == 4);
 
 }
