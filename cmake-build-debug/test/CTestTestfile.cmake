fbc2c68d9dcbf36405199f9e45750231674919f4
Kevin <kevinhmai@outlook.com>
2019-11-16 18:32:16 -0800

    first commit

---
+++ cmake-build-debug/test/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/mclovin/Desktop/homework5/problem_2/test
+# Build directory: /home/mclovin/Desktop/homework5/problem_2/cmake-build-debug/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(BinarySearchTreeTests "/home/mclovin/Desktop/homework5/problem_2/bin/binary_search_tests")
+set_tests_properties(BinarySearchTreeTests PROPERTIES  _BACKTRACE_TRIPLES "/home/mclovin/Desktop/homework5/problem_2/test/CMakeLists.txt;10;add_test;/home/mclovin/Desktop/homework5/problem_2/test/CMakeLists.txt;0;")
